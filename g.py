184e6c9ae9455ebfc575cba9d84f1319d808167b
ShockedQuartz <30025742+ShockedQuartz@users.noreply.github.com>
2026-06-30 01:25:59 +0100

    Add entity tags and global variables module
    title

---
+++ g.py
@@ -0,0 +1,9 @@
+#Global variables
+from __future__ import annotations
+
+import tcod.context
+import tcod.ecs
+
+context: tcod.context.Context
+
+world: tcod.ecs.Registry
---
+++ game/tags.py
@@ -0,0 +1,10 @@
+#Tags to specify the types of entities in the game
+from __future__ import annotations
+
+from typing import Final
+
+IS_PLAYER: Final = "isPlayer"
+
+IS_ACTOR: Final = "isActor"
+
+IS_ITEM: Final = "isItem"
